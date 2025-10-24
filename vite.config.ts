74ba23fc7c8abe07432f3b40449c1d93499769a7
[dyad] <canalvendas54@gmail.com>
2025-10-24 18:48:03 -0300

    [dyad] wrote 5 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,18 @@ export default defineConfig(() => ({
     },
   },
   envPrefix: 'VITE_',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['@radix-ui/react-dialog', '@radix-ui/react-dropdown-menu', '@radix-ui/react-tabs'],
+          utils: ['clsx', 'tailwind-merge'],
+        }
+      }
+    }
+  }
 }));
